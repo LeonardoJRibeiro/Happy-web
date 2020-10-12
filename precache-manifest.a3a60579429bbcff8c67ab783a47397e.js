dbebb65c2edd8f4cd37614c4a3f716b86af7e490
Leonardo Jardim Ribeiro <leonardojr410@gmail.com>
2020-10-12 17:35:16 -0300

    Updates

---
+++ precache-manifest.a3a60579429bbcff8c67ab783a47397e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb76f953602ff2217ce490e0c8d840a",
+    "url": "/happy/index.html"
+  },
+  {
+    "revision": "c97096b37d51a515964b",
+    "url": "/happy/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "105232799eb815a388c6",
+    "url": "/happy/static/css/main.bc8e346a.chunk.css"
+  },
+  {
+    "revision": "c97096b37d51a515964b",
+    "url": "/happy/static/js/2.5faba2a5.chunk.js"
+  },
+  {
+    "revision": "0580f66cff7a7b2e7b5c118fd842bcfd",
+    "url": "/happy/static/js/2.5faba2a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "105232799eb815a388c6",
+    "url": "/happy/static/js/main.d05277e4.chunk.js"
+  },
+  {
+    "revision": "21f0cf04f9acf599dd01",
+    "url": "/happy/static/js/runtime-main.4b200911.js"
+  },
+  {
+    "revision": "91c8d1af04a8fbdde51d2b16c5471b15",
+    "url": "/happy/static/media/landing.91c8d1af.svg"
+  },
+  {
+    "revision": "1653c64706c95e2e85a07cc755abf8ba",
+    "url": "/happy/static/media/logo.1653c647.svg"
+  },
+  {
+    "revision": "658b83029253fe8e316b8f364db7b424",
+    "url": "/happy/static/media/map-marker.658b8302.svg"
+  }
+]);
